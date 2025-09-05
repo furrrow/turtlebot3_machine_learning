8b874ab6b5c768d1394f7ce9130f99e8cb1648bf
jim <jianyu234@gmail.com>
2025-09-05 14:48:17 -0400

    commit

---
+++ turtlebot3_dqn/turtlebot3_dqn/read_csv_test.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('rac.csv', newline='') as csvfile:
+
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+    for row in spamreader:
+
+        print(', '.join(row))
--- turtlebot3_dqn/turtlebot3_dqn/read_h5_file.py
+++ turtlebot3_dqn/turtlebot3_dqn/read_h5_file.py
@@ -7,10 +7,12 @@ h5py docs: https://docs.h5py.org/en/stable/quick.html
 """
 import h5py
 import numpy as np
-# from tqdm import tqdm
+import csv
+from tqdm import tqdm
 
 def main():
     h5_path = "Aug27_traces.hdf5" # change this!
+    trace_number = 0
     f = h5py.File(h5_path, "r")
     keys_list = list(f.keys())
     # ['action', 'dones', 'dt', 'next_state', 'reward', 'state', 'step', 'theta', 'x', 'y']
@@ -24,12 +26,26 @@ def main():
 
     # one may directly access an element
     print(f['x'][100])
-
+    fieldnames = ['x', 'y', 'theta', 'dt', 'terminal']
+    terminal = False
     # or we can do a loop, tqdm might make it pretty
-    for i in range(num_entries):
-        pose_entry = (f['x'][i], f['y'][i], f['theta'][i])
-        print(pose_entry)
-        break
+    i = 0
+    csv_entries = 0
+    while i < num_entries:
+        csv_file_name = f"trace_records{trace_number}.csv"
+        with open(csv_file_name, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            terminal = abs(f['reward'][i]) > 45
+            data = (f['x'][i], f['y'][i], f['theta'][i], f['dt'][i])
+            writer.writerow(data)
+            csv_entries += 1
+            i += 1
+            if terminal:
+                print(f"{csv_entries} saved to {csv_file_name}")
+                trace_number+= 1
+                break
+
+
 
     # **analysis** taking a look at how many episodes, success rate, and cumulative reward for each.
     rewards_list = []
