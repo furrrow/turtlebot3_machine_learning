e202a8fb480fa12174649f9529070af3db2a8024
jim <jianyu234@gmail.com>
2025-09-05 21:16:22 -0400

    updated read_h5_file.py to extract csvs for individual traces

--- turtlebot3_dqn/turtlebot3_dqn/read_h5_file.py
+++ turtlebot3_dqn/turtlebot3_dqn/read_h5_file.py
@@ -11,58 +11,49 @@ import csv
 from tqdm import tqdm
 
 def main():
-    h5_path = "Aug27_traces.hdf5" # change this!
+    h5_path = "aug27_traces_greedy.hdf5" # change this!
     trace_number = 0
     f = h5py.File(h5_path, "r")
     keys_list = list(f.keys())
     # ['action', 'dones', 'dt', 'next_state', 'reward', 'state', 'step', 'theta', 'x', 'y']
     print(keys_list)
     # some metadata can be stored as well
-    for meta_key in f.attrs:
-        print(f"metadata - {meta_key}:")
-        print(f.attrs[meta_key])
+    # for meta_key in f.attrs:
+    #     print(f"metadata - {meta_key}:")
+    #     print(f.attrs[meta_key])
     num_entries = f[keys_list[-1]].len()
     print(f"total of {num_entries} entries")
 
     # one may directly access an element
     print(f['x'][100])
-    fieldnames = ['x', 'y', 'theta', 'dt', 'terminal']
-    terminal = False
-    # or we can do a loop, tqdm might make it pretty
-    i = 0
-    csv_entries = 0
-    while i < num_entries:
-        csv_file_name = f"trace_records{trace_number}.csv"
-        with open(csv_file_name, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            terminal = abs(f['reward'][i]) > 45
-            data = (f['x'][i], f['y'][i], f['theta'][i], f['dt'][i])
-            writer.writerow(data)
-            csv_entries += 1
-            i += 1
-            if terminal:
-                print(f"{csv_entries} saved to {csv_file_name}")
-                trace_number+= 1
-                break
-
-
 
     # **analysis** taking a look at how many episodes, success rate, and cumulative reward for each.
     rewards_list = []
-    cumulative_reward = 0
     success_tally = []
-    for i in range(num_entries - 1):
-        cumulative_reward += f['reward'][i][0]
-        if f['step'][i+1][0] == 0:
+    last_i = 0
+    keys_to_extract = ['step', 'x', 'y', 'theta', 'dt']
+    for i in range(num_entries):
+        if i == num_entries-1:
+            terminal = True
+        elif f['step'][i + 1][0] == 0:
+            terminal = True
+        else:
+            terminal = False
+        if terminal:
+            data = [f[key][last_i:i+1] for key in keys_to_extract]
+            csv_file_name = f"../csvs/trace_records{len(rewards_list)}.csv"
+            data = np.array(data).squeeze(-1).T
+            np.savetxt(csv_file_name, data, delimiter=",")
+            cumulative_reward = sum(f['reward'][last_i:i+1])[0]
+            csv_entries = i - last_i + 1
             success_tally.append(f['reward'][i][0] > 90)
-            print(f"end of epoch {len(rewards_list)}, score: {cumulative_reward:.3f}")
             rewards_list.append(cumulative_reward)
-            cumulative_reward = 0
-    cumulative_reward += f['reward'][num_entries-1][0]
-    print(f"end of epoch {len(rewards_list)}, score: {cumulative_reward:.3f}")
-    rewards_list.append(cumulative_reward)
+            print(f"end of epoch {len(rewards_list)}, score: {cumulative_reward:.3f}, {csv_entries} steps saved to {csv_file_name}")
+            continue
     success_rate = np.sum(np.array(success_tally) > 0) / len(rewards_list)
     print(f"success rate: {success_rate:.3f}")
+    print(f"keys extracted:{keys_to_extract}")
+
 
 if __name__ == '__main__':
     main()
